cdb9cfee122a155b67b68bb591a301723ee85a72
liukai <your_email@abc.example>
2023-01-10 10:26:48 +0800

    add configs

---
+++ projects/group_fisher/configs/mmdet/group-fisher-finetune_retinanet_resnet50_8xb2_coco.py
@@ -0,0 +1,26 @@
+_base_ = 'mmdet::retinanet/retinanet_r50_fpn_1x_coco.py'
+custom_imports = dict(imports=['projects.group_fiser'])
+
+architecture = _base_.model
+architecture.backbone.frozen_stages = -1
+# `pruned_path` need to be updated.
+pruned_path = '/your/local/pruned/checkpoint/path'
+
+model = dict(
+    _delete_=True,
+    _scope_='mmrazor',
+    type='GroupFisherAlgorithm',
+    architecture=architecture,
+    pruning=False,
+    mutator=dict(
+        type='GroupFisherChannelMutator',
+        parse_cfg=dict(type='ChannelAnalyzer', tracer_type='FxTracer'),
+        channel_unit_cfg=dict(
+            type='GroupFisherChannelUnit',
+            default_args=dict(choice_mode='ratio'))),
+    init_cfg=dict(type='Pretrained', checkpoint=pruned_path),
+)
+
+optim_wrapper = dict(type='OptimWrapper', optimizer=dict(lr=0.01))
+
+find_unused_parameters = True
