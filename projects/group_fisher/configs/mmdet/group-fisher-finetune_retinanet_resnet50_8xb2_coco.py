880fcc32bfbb3dfe071ff0ebd6b3a5c7602cb08f
zhangzhongyu <314647732@qq.com>
2023-01-13 15:01:13 +0800

    update configs

--- projects/group_fisher/configs/mmdet/group-fisher-finetune_retinanet_resnet50_8xb2_coco.py
+++ projects/group_fisher/configs/mmdet/group-fisher-finetune_retinanet_resnet50_8xb2_coco.py
@@ -15,12 +15,8 @@ model = dict(
     mutator=dict(
         type='GroupFisherChannelMutator',
         parse_cfg=dict(type='ChannelAnalyzer', tracer_type='FxTracer'),
-        channel_unit_cfg=dict(
-            type='GroupFisherChannelUnit',
-            default_args=dict(choice_mode='ratio'))),
+        channel_unit_cfg=dict(type='GroupFisherChannelUnit')),
     init_cfg=dict(type='Pretrained', checkpoint=pruned_path),
 )
 
 optim_wrapper = dict(type='OptimWrapper', optimizer=dict(lr=0.01))
-
-find_unused_parameters = True
--- projects/group_fisher/configs/mmdet/group-fisher-pruning_retinanet_resnet50_8xb2_coco.py
+++ projects/group_fisher/configs/mmdet/group-fisher-pruning_retinanet_resnet50_8xb2_coco.py
@@ -27,5 +27,3 @@ model = dict(
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001))
-
-find_unused_parameters = True
