e6bc6fd741f46596d049ec266e419b930b19ebc3
liukai <your_email@abc.example>
2023-01-29 14:03:01 +0800

    update resnet50 configs

--- projects/group_fisher/configs/mmcls/resnet_group_fisher_prune.py
+++ projects/group_fisher/configs/mmcls/resnet_group_fisher_prune.py
@@ -1,5 +1,5 @@
 _base_ = 'mmcls::resnet/resnet50_8xb32_in1k.py'
-custom_imports = dict(imports=['projects.group_fisher'])
+custom_imports = dict(imports=['projects'])
 architecture = _base_.model
 pretrained_path = 'https://download.openmmlab.com/mmclassification/v0/resnet/resnet50_8xb32_in1k_20210831-ea4938fc.pth'  # noqa
 architecture.init_cfg = dict(type='Pretrained', checkpoint=pretrained_path)
@@ -13,25 +13,51 @@ model = dict(
     _scope_='mmrazor',
     type='GroupFisherAlgorithm',
     architecture=architecture,
-    interval=10,
-    delta='flops',
-    save_ckpt_delta_thr=[0.75, 0.5, 0.25],
-    esitmator=dict(
-        type='ResourceEstimator',
-        flops_params_cfg=dict(input_shape=(1, 3, 224, 224), )),
+    interval=25,
     mutator=dict(
         type='GroupFisherChannelMutator',
         parse_cfg=dict(type='ChannelAnalyzer', tracer_type='FxTracer'),
         channel_unit_cfg=dict(
             type='GroupFisherChannelUnit',
             detla_type='act',
-            default_args=dict(detla_type='act', )),
-        min_ratio=0.0,
+        ),
     ),
 )
+model_wrapper_cfg = dict(
+    type='mmrazor.GroupFisherDDP',
+    broadcast_buffers=False,
+)
+# update optimizer
 
-optim_wrapper = dict(optimizer=dict(lr=0.001))  # origin 0.1
+optim_wrapper = dict(optimizer=dict(lr=0.004, ))
+param_scheduler = None
+
 custom_hooks = [
     dict(type='mmrazor.PruningStructureHook'),
-    dict(type='mmrazor.ResourceInfoHook'),
+    dict(
+        type='mmrazor.ResourceInfoHook',
+        interval=25,
+        demo_input=dict(
+            type='mmrazor.DefaultDemoInput',
+            input_shape=[1, 3, 224, 224],
+        ),
+        save_ckpt_delta_thr=[0.75, 0.50],
+    ),
 ]
+
+# original
+"""
+optim_wrapper = dict(
+    optimizer=dict(
+        type='SGD',
+        lr=0.1,
+        momentum=0.9,
+        weight_decay=0.0001,
+        _scope_='mmcls'))
+param_scheduler = dict(
+    type='MultiStepLR',
+    by_epoch=True,
+    milestones=[30, 60, 90],
+    gamma=0.1,
+    _scope_='mmcls')
+"""
